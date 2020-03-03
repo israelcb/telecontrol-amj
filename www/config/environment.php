6cd2360a117e8ec139e6f7bbe5920c05e0a8803e
rozcovo <israel.batista@univem.edu.br>
2020-03-03 19:40:35 -0300

    Corrigindo erro de codificação dos imports de CSS e JS

--- www/config/environment.php
+++ www/config/environment.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BASE_URL', 'http://localhost/amj/www/');
+define('BASE_URL', 'http://localhost/amj/www');
 define('SGBD_HOST', '127.0.0.1');
 define('SGBD_PORT', '5432');
 define('SGBD_DATABASE', 'amj');
